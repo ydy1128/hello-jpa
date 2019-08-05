b5558de1f0aa6ae24a8b9309648100f2c895b433
ydy1128 <ydy1128@gmail.com>
2019-08-05 22:40:41 +0900

    initial

---
+++ src/main/java/com/jyoon/study/hellojpa/HelloJpaApplication.java
@@ -0,0 +1,11 @@
+package com.jyoon.study.hellojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJpaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJpaApplication.class, args);
+	}
+}
